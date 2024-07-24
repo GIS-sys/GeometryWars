53a938ab4faf6842405fbb8223905cac7d0e47bf
GISwarm <gordeiegorov@gmail.com>
2024-07-24 10:24:52 +0300

    main game scene - base

--- geometry_wars/scene/MainGameScene.cpp
+++ geometry_wars/scene/MainGameScene.cpp
@@ -1,13 +1,29 @@
 #include "MainGameScene.h"
 
 
-MainGameScene::~MainGameScene() {}
+MainGameScene::~MainGameScene() {
+    for (GameObject* go : enemies) delete go;
+    for (GameObject* go : projectiles) delete go;
+}
 
 MainGameScene::MainGameScene() {}
 
-void MainGameScene::draw(GameBuffer buffer) {}
+void MainGameScene::draw(GameBuffer buffer) {
+    prev_buffer_width = buffer.width;
+    prev_buffer_height = buffer.height;
+    battlefield.draw(&camera);
+    player.draw(&camera);
+    for (GameObject* go : enemies) go->draw(buffer);
+    for (GameObject* go : projectiles) go->draw(buffer);
+}
 
 GameScene::Type MainGameScene::act(float dt) {
+    // move player TODO
+    // move enemies toward player TODO
+    // move projectiles TODO
+    // fire new projectiles TODO
+    // spawn new enemies TODO
+    // move camera TODO
     // exit to main menu
     if (engine_is_key_pressed(keys::K_RETURN)) {
         return GameScene::Type::main_menu;
--- geometry_wars/scene/MainGameScene.h
+++ geometry_wars/scene/MainGameScene.h
@@ -4,6 +4,15 @@
 
 class MainGameScene : public GameScene {
   private:
+    Camera camera;
+    Battlefield battlefield;
+    GOPlayer player;
+    std::vector<GameObject*> enemies;
+    std::vector<GameObject*> projectiles;
+    GOLabel score;
+
+    int prev_buffer_height = 0;
+    int prev_buffer_width = 0;
 
   public:
     ~MainGameScene();
